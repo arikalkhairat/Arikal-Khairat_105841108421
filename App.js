7f58dc159223ad024e67bb735dac25fb76021c46
arikalkhairat <105841108421@student.unismuh.ac.id>
2024-05-29 14:52:52 +0800

    Lab2

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+
 const App = () => {
   return (
-    <View style={{flex : 1, 
+    <View style={{ flex: 1 }}>
+    <View style={{
+      flex: 1,
+      alignItems: 'flex-end', //BARIS
+      justifyContent: 'center', //KOLOM
+      flexDirection:'row',
+      marginRight: 20,
+      marginBottom: 100
     }}>
-      <View style={{ flex : 1, flexDirection:"row", backgroundColor: 'white'}}>
-        <View style={{ flex : 1, flexDirection: 1, backgroundColor: 'white'}}>
-        </View>
-        <View style={{ flex : 1, flexDirection: 1, backgroundColor: 'blue'}}>
-        </View>
+      <View style={{
+        backgroundColor: 'fuchsia', width: 150, height: 80,
+        justifyContent: 'center',
+        borderRadius: 20,
+        marginRight: 20
+    }}>
+  
+      <Text style={{
+        fontsize: 100, 
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+        
+      }}> 
+      LOGIN
+      </Text>
       </View>
-      <View style={{ flex : 1, backgroundColor: 'red'}}>
-    </View>
-    <View style={{ flex : 1, backgroundColor: 'green'}}>
-    </View>
-  </View>
+      <View style={{
+        backgroundColor: 'fuchsia', width: 150, height: 80,
+        justifyContent: 'center',
+        borderRadius: 20,
+        marginLeft: 20
+    }}>
+  
+      <Text style={{
+        fontsize: 100, 
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+        
+      }}> 
+      REGISTER
+      </Text>
+      </View>
+      
+      </View>
+      </View>
+    
+    
   )
 }
 export default App
-
