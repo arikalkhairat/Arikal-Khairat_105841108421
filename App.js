f24d614c36295bdc159b8fdb510ee725ccefe905
arikalkhairat <105841108421@student.unismuh.ac.id>
2024-06-28 14:31:11 +0800

    OKESI

--- App.js
+++ App.js
@@ -1,50 +1,28 @@
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
-import React from 'react'
-import ButtonCustom from './component/button/button'
-import TextInputCustom from './component/TextInput'
-import CustomText from './component/Text'
-import Icon from './component/Icon'
+import { useFonts } from 'expo-font';
+import { Text, View } from 'react-native';
 
 
-const SignUp = () => { 
+export default function App() {
+  const [dapatFont] = useFonts({
+    'MetroBlack': require('./assets/font/Metropolis-Black.otf'),
+    'MetroBold': require('./assets/font/Metropolis-Bold.otf'),
+    'MetroLight': require('./assets/font/Metropolis-Light.otf'),
+    'MetroMedium': require('./assets/font/Metropolis-Medium.otf'),
+    'MetroSemiBold': require('./assets/font/Metropolis-SemiBold.otf'),
+  });
+
+  if(!dapatFont) {
+    return <Text>Font Tidak ditemukan</Text>
+  }
+
   return (
-    <View style={{ flex:1, backgroundColor:'#FAEBE5'}}>
-      <View style={{
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-      marginTop: 100,
-      marginLeft: 30,
-      marginBottom:50
-    }}>
-      <Text style={{
-      fontSize: 35,
-      color: '#951332',
-      fontWeight: 'bold'
-    
-    }}>
-      SignUp
-    </Text>   
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style= {{ fontFamily: 'MetroBlack'}}>Font Metropolis Black</Text>
+      <Text style= {{ fontFamily: 'MetroBold'}}>Font Metropolis Bold</Text>
+      <Text style= {{ fontFamily: 'MetroLight'}}>Font Metropolis Light</Text>
+      <Text style= {{ fontFamily: 'MetroMedium'}}>Font Metropolis Medium</Text>
+      <Text style= {{ fontFamily: 'MetroSemiBold'}}>Font Metropolis Semi Bold</Text>
+      <Text>Font Biasa</Text>
     </View>
-    <View style={{ alignItems:'center', justifyContent:'center'}}>
-      
-     <TextInputCustom name="Username" color="#B7919A" label="Nama" />
-     <TextInputCustom name="email" color="#B7919A" label="Nama"/>
-     <TextInputCustom name="Password" color="#B7919A" label="Nama" />
-     <View style={{marginLeft:150}}>
-     <CustomText text="Already have an account?" />
-     </View>
-     <ButtonCustom color="#CC3E5F" text="SignUp"/>
-     <CustomText text="Or sign up with social account" style={{ marginBottom:50}} />
-
-     <View style={{
-      flexDirection:'row'}}>
-      <Icon imageSource={require('./assets/go.png')} />
-      <Icon imageSource={require('./assets/fb.png')} />
-     </View>
-     
-    </View>
-    </View>
-  )  
+  );
 }
-
-export default SignUp
